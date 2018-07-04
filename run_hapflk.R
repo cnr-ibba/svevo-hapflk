f0be6e5d4171d1ed14b8db0c3aaa4a7bfe41f298
Paolo Cozzi <bunop@libero.it>
2018-07-04 19:50:47 +0200

    Fixing paths
    Using relative paths

--- run_hapflk.R
+++ run_hapflk.R
@@ -4,21 +4,21 @@
 # DEW-DWL
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl", i)
+  cmd <- paste0("hapflk --bfile hapflk_dew_dwl/dew_dwl --kinship hapflk_dew_dwl/dew_dwl_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dew_dwl/dew_dwl", i)
   system(cmd, wait = T)
 }
 
 # DWL-DWC
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc", i)
+  cmd <- paste0("hapflk --bfile hapflk_dwl_dwc/dwl_dwc --kinship hapflk_dwl_dwc/dwl_dwc_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dwl_dwc/dwl_dwc", i)
   system(cmd, wait = T)
 }
 
 # WEW-DEW
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew", i)
+  cmd <- paste0("hapflk --bfile hapflk_wew_dew/wew_dew --kinship hapflk_wew_dew/wew_dew_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_wew_dew/wew_dew", i)
   system(cmd, wait = T)
 }
 
@@ -27,21 +27,21 @@ for(i in ch){
 # DEW-DWL
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop", i)
+  cmd <- paste0("hapflk --bfile hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop --kinship hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop", i)
   system(cmd, wait = T)
 }
 
 # DWL-DWC
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop", i)
+  cmd <- paste0("hapflk --bfile hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop --kinship hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop", i)
   system(cmd, wait = T)
 }
 
 # WEW-DEW
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/sub_pop/wew_dew_sub_pop --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/sub_pop/wew_dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/sub_pop/wew_dew_sub_pop", i)
+  cmd <- paste0("hapflk --bfile hapflk_wew_dew/sub_pop/wew_dew_sub_pop --kinship hapflk_wew_dew/sub_pop/wew_dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_wew_dew/sub_pop/wew_dew_sub_pop", i)
   system(cmd, wait = T)
 }
 
@@ -49,14 +49,14 @@ for(i in ch){
 # DEW
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dew_sub_pop --kinship /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dew_sub_pop", i)
+  cmd <- paste0("hapflk --bfile DEW_and_DWL/dew_sub_pop --kinship DEW_and_DWL/dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p DEW_and_DWL/dew_sub_pop", i)
   system(cmd, wait = T)
 }
 
 # DWL
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
-  cmd <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dwl_sub_pop --kinship /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/DEW_and_DWL/dwl_sub_pop", i)
+  cmd <- paste0("hapflk --bfile DEW_and_DWL/dwl_sub_pop --kinship DEW_and_DWL/dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p DEW_and_DWL/dwl_sub_pop", i)
   system(cmd, wait = T)
 }
 
@@ -66,9 +66,9 @@ for(i in ch){
 
 #ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 #for(i in ch){
-#  cmd1 <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl_fij.txt -K 15 --ncpu=2 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dew_dwl/dew_dwl", i)
-#  cmd2 <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc_fij.txt -K 15 --ncpu=2 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_dwl_dwc/dwl_dwc", i)
-#  cmd3 <- paste0("hapflk --bfile /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew --kinship /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew_fij.txt -K 15 --ncpu=2 --chr ", i," -p /home/danara/Documents/hierfstat/hapflk/hapflk_wew_dew/wew_dew", i)
+#  cmd1 <- paste0("hapflk --bfile hapflk_dew_dwl/dew_dwl --kinship hapflk_dew_dwl/dew_dwl_fij.txt -K 15 --ncpu=2 --chr ", i," -p hapflk_dew_dwl/dew_dwl", i)
+#  cmd2 <- paste0("hapflk --bfile hapflk_dwl_dwc/dwl_dwc --kinship hapflk_dwl_dwc/dwl_dwc_fij.txt -K 15 --ncpu=2 --chr ", i," -p hapflk_dwl_dwc/dwl_dwc", i)
+#  cmd3 <- paste0("hapflk --bfile hapflk_wew_dew/wew_dew --kinship hapflk_wew_dew/wew_dew_fij.txt -K 15 --ncpu=2 --chr ", i," -p hapflk_wew_dew/wew_dew", i)
 #  system(cmd1, wait = T)
 #  system(cmd2, wait = T)
 #  system(cmd3, wait = T)
