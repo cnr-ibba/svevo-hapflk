66c8e171504ffdaadc7469d9e8cfa441ddc24547
Paolo Cozzi <bunop@libero.it>
2018-07-05 12:51:54 +0200

    Minor fix

--- run_hapflk.R
+++ run_hapflk.R
@@ -13,6 +13,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile hapflk_dwl_dwc/dwl_dwc --kinship hapflk_dwl_dwc/dwl_dwc_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dwl_dwc/results/dwl_dwc", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -20,6 +21,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile hapflk_wew_dew/wew_dew --kinship hapflk_wew_dew/wew_dew_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_wew_dew/results/wew_dew", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -29,6 +31,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop --kinship hapflk_dew_dwl/sub_pop/dew_dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dew_dwl/sub_pop/results/dew_dwl_sub_pop", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -36,6 +39,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop --kinship hapflk_dwl_dwc/sub_pop/dwl_dwc_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_dwl_dwc/sub_pop/results/dwl_dwc_sub_pop", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -43,6 +47,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile hapflk_wew_dew/sub_pop/wew_dew_sub_pop --kinship hapflk_wew_dew/sub_pop/wew_dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p hapflk_wew_dew/sub_pop/results/wew_dew_sub_pop", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -51,6 +56,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile DEW_and_DWL/dew_sub_pop --kinship DEW_and_DWL/dew_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p DEW_and_DWL/results/dew_sub_pop", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
@@ -58,6 +64,7 @@ for(i in ch){
 ch <- paste0(rep(1:7, 2), c(rep("A",7), rep("B",7)))
 for(i in ch){
   cmd <- paste0("hapflk --bfile DEW_and_DWL/dwl_sub_pop --kinship DEW_and_DWL/dwl_sub_pop_fij.txt -K 10 --ncpu=6 --annot --nfit=50 --chr ", i," -p DEW_and_DWL/results/dwl_sub_pop", i)
+  print(cmd)
   # system(cmd, wait = T)
 }
 
